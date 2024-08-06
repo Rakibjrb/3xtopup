e05f25c604c440abb59e796c62434f1052245eb2
rakibjrb <rakibjrb572157@gmail.com>
2024-08-06 21:41:09 +0600

    added sign-up and sign-in page and make responsive homepage

---
+++ src/app/(auth)/sign-up/page.js
@@ -0,0 +1,151 @@
+"use client";
+
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { ImSpinner9 } from "react-icons/im";
+import Link from "next/link";
+import "./signup.css";
+
+export default function SignUp() {
+  const [loading, setLoading] = useState(false);
+  const [showPass, setShowPass] = useState(false);
+
+  const handleCreateAccount = (e) => {
+    e.preventDefault();
+
+    const info = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+      repassword: e.target.repassword.value,
+    };
+
+    const regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/;
+    if (!regex.test(info.password)) {
+      toast(
+        "Please make sure you entered uppercase, \n\n number and one spacial character.",
+        {
+          duration: 8000,
+        }
+      );
+      return;
+    }
+
+    if (info.password !== info.repassword) {
+      toast.error("Re-type password doesn't matched");
+      return;
+    }
+
+    setLoading(true);
+
+    console.log(info);
+
+    setLoading(false);
+    toast.success("Account creation successful");
+    e.target.name.value = "";
+    e.target.email.value = "";
+    e.target.password.value = "";
+    e.target.repassword.value = "";
+  };
+
+  return (
+    <div className="px-4 py-6 md:px-0 flex justify-center items-center w-full min-h-screen signup-container">
+      <form
+        onSubmit={handleCreateAccount}
+        className="flex flex-col gap-5 p-8 border border-[#f948b2] rounded-lg"
+      >
+        <div>
+          <label htmlFor="name" className="text-white">
+            Enter Full Name
+            <input
+              name="name"
+              className="mt-3 w-full border-2 border-[#f948b2] p-2 rounded-md outline-none bg-transparent placeholder:text-white"
+              type="text"
+              placeholder="Enter your name"
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email" className="text-white">
+            Enter your email
+            <input
+              name="email"
+              className="mt-3 w-full border-2 border-[#f948b2] p-2 rounded-md outline-none bg-transparent placeholder:text-white"
+              type="email"
+              placeholder="Enter email here"
+              required
+            />
+          </label>
+        </div>
+        <div className="relative">
+          <label htmlFor="password" className="text-white">
+            password
+            <input
+              name="password"
+              className="mt-3 w-full border-2 border-[#f948b2] p-2 rounded-md outline-none bg-transparent placeholder:text-white"
+              type={`${showPass ? "text" : "password"}`}
+              placeholder="type password here"
+              required
+            />
+          </label>
+          <div
+            onClick={() => setShowPass(!showPass)}
+            className="inline-block cursor-pointer absolute top-12 right-3 text-white"
+          >
+            {showPass ? (
+              <IoMdEyeOff className="text-xl" />
+            ) : (
+              <IoMdEye className="text-xl" />
+            )}
+          </div>
+        </div>
+        <div className="relative">
+          <label htmlFor="repassword" className="text-white">
+            Re-Type password
+            <input
+              name="repassword"
+              className="mt-3 w-full border-2 border-[#f948b2] p-2 rounded-md outline-none bg-transparent placeholder:text-white"
+              type={`${showPass ? "text" : "password"}`}
+              placeholder="re-type password here"
+              required
+            />
+          </label>
+          <div
+            onClick={() => setShowPass(!showPass)}
+            className="inline-block cursor-pointer absolute top-12 right-3 text-white"
+          >
+            {showPass ? (
+              <IoMdEyeOff className="text-xl" />
+            ) : (
+              <IoMdEye className="text-xl" />
+            )}
+          </div>
+        </div>
+        <div className="w-full">
+          <button
+            type="submit"
+            className="w-full flex justify-center bg-[#f948b2] hover:bg-gray-300 py-3 rounded-md uppercase transition-colors duration-200"
+          >
+            {loading ? (
+              <ImSpinner9 className="text-xl animate-spin" />
+            ) : (
+              "Create Account"
+            )}
+          </button>
+          <h3 className="text-center text-xl my-3 text-white">
+            Already have an account?
+          </h3>
+          <Link
+            href={"/sign-in"}
+            className="w-full flex justify-center bg-[#8758f1] hover:bg-gray-300 py-3 rounded-md uppercase transition-colors duration-200"
+          >
+            Sign In
+          </Link>
+        </div>
+      </form>
+      <Toaster position="top-center" />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <div className="banner min-h-screen text-white">
         <div className="max-w-7xl mx-auto">
-          <nav className="w-full flex justify-between items-center pt-8">
+          <nav className="w-full flex flex-col-reverse md:flex-row gap-y-4 md:gap-y-0 justify-between items-center md:px-3 xl:px-0 pt-8">
             <div className="flex items-center">
               <h2 className="font-bold text-3xl">
                 <span className="color1">3X</span>TOPUP
@@ -22,7 +22,7 @@ export default function Home() {
                   About Us
                 </Link>
               </li>
-              <li className="text-white py-2 px-5 rounded-md flex gap-x-3 bg-[#ff42a6]">
+              <li className="text-white py-2 px-5 rounded-md flex gap-x-3 bg-[#ff42a6] hover:bg-gray-600 transition-colors duration-200">
                 Follow Us On{" "}
                 <Link href="">
                   <FaYoutube className="text-xl" />
@@ -34,9 +34,9 @@ export default function Home() {
             </ul>
           </nav>
 
-          <div className="min-h-[calc(100vh-72px)] flex justify-between items-center">
-            <div className="w-3/4">
-              <h1 className="styled-text text-[74px] font-extrabold">
+          <div className="min-h-[calc(100vh-140px)] md:min-h-[calc(100vh-110px)] flex justify-between items-center px-3 xl:px-0">
+            <div className="lg:w-3/4">
+              <h1 className="styled-text text-6xl  md:text-7xl font-extrabold text-center md:text-left">
                 DIAMOND TOP UP
               </h1>
               <h2 className="uppercase text-4xl font-bold">
@@ -47,13 +47,19 @@ export default function Home() {
                 <li>Game ID BUY/SELL</li>
                 <li>24/7 Support</li>
               </ul>
-              <div className="flex gap-x-6">
-                <button className="px-5 py-3 bg-[#ff42a5] rounded-full uppercase text-white">
-                  Get Started
-                </button>
-                <button className="px-5 py-3 bg-[#3e9ee3] rounded-full uppercase text-white">
+              <div className="flex gap-x-3 md:gap-x-6">
+                <Link
+                  href={"/sign-up"}
+                  className="px-5 py-3 bg-[#ff42a5] rounded-full uppercase text-white hover:bg-gray-600 transition-colors duration-200"
+                >
+                  Create Account
+                </Link>
+                <Link
+                  href={"/sign-in"}
+                  className="px-5 py-3 bg-[#3e9ee3] rounded-full uppercase text-white hover:bg-gray-600 transition-colors duration-200"
+                >
                   Sign In
-                </button>
+                </Link>
               </div>
             </div>
           </div>
